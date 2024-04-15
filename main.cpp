9c626962f9ce07a558e9723ae365e78cc1f8265c
Hiisam17 <ktoan1702@gmail.com>
2024-04-15 21:47:16 +0700

    Generated text time count for game

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 #include "player_object.h"
 #include "threats.h"
 #include "check_attack.h"
+#include "text_object.h"
 
 base_object g_background;
+TTF_Font* font_time = NULL;
 
 bool InitData() 
 {
@@ -48,6 +50,18 @@ bool InitData()
                 success = false;
             }
         }
+
+        if (TTF_Init() == -1)
+        {
+            success = false;
+        }
+
+        font_time = TTF_OpenFont("font//dlxfont_.ttf", 20);
+        if (font_time == NULL)
+        {
+            success = false;
+        }
+        
     }
 
     return success;
@@ -148,8 +162,8 @@ int main(int argc, char* argv[])
 
     std::vector<ThreatsObject*> threats_list = MakeThreatsList();
 
-    int num_die = 0;
-    int hp_ = 3;
+    TextObject time_game;
+    time_game.SetColor(TextObject::WHITE_TEXT);
 
     bool is_quit = false;
 
@@ -211,13 +225,33 @@ int main(int argc, char* argv[])
 
         if (life_check == true)
         {
+            is_quit = true;
             break;
         }
 
         bool is_game_over = p_player.is_game_over(map_data, p_player);
         if (is_game_over == true)
         {
+            is_quit = true;
             break;
+        }
+
+        std::string str_time = "Time: ";
+        Uint32 time_val = SDL_GetTicks() / 1000;
+        Uint32 val_time = 300 - time_val;
+        if (val_time <= 0)
+        {
+            is_quit = true;
+            break;
+        }
+        else
+        {
+            std::string str_val = std::to_string(val_time);
+            str_time += str_val;
+
+            time_game.SetText(str_time);
+            time_game.LoadFromRenderText(font_time, g_screen);
+            time_game.RenderText(g_screen, SCREEN_WIDTH - 200, 15);
         }
 
         SDL_RenderPresent(g_screen);
